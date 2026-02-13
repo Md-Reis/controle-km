0e8c76abbcfc1d1df9909ad1820d7062e10f7bf5
Marcelo Reis <marcelo.mdreis@gmail.com>
2026-02-13 15:08:49 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "controle-km-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./assets/logo.webp",
+        "./assets/icon-192.png",
+        "./assets/icon-512.png"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
